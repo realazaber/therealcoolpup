6a3784293d48a688781df71eae19bf1d8c8dc21d
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-07 03:44:52 +1100

    animate main text and about section

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,9 @@
-const timeline = gsap.timeline({ defaults: {duration: 2}});
-timeline
-    .from('#welcomeText', { x: '100%', ease: 'bounce'})
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('#welcomeText', { x: '100%', duration: 2, ease: 'elastic'});
+
+gsap.from('#profileImg', {scrollTrigger: "#aboutEndWave", opacity: 0, duration: 2, ease: 'power1'});
+gsap.from('#aboutMe', {scrollTrigger: "#aboutEndWave", opacity: 0, duration: 2, ease: 'power1'});
+
+
     
