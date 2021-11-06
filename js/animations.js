4f3cec96cdf9f50198a58502e6df0a1d7f70ffb0
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-07 03:57:38 +1100

    fixed images

--- js/animations.js
+++ js/animations.js
@@ -5,5 +5,9 @@ gsap.from('#welcomeText', { x: '100%', duration: 2, ease: 'elastic'});
 gsap.from('#profileImg', {scrollTrigger: "#aboutEndWave", opacity: 0, duration: 1, ease: 'power1'});
 gsap.from('#aboutMe', {scrollTrigger: "#aboutEndWave", opacity: 0, duration: 3, ease: 'power1'});
 
-gsap.from('.project', {scrollTrigger: "#contact", y: '300%', stagger: 0.3, ease: 'power1'});
+gsap.from('.project', {scrollTrigger: "#projects", y: '300%', stagger: 0.3, ease: 'power1'});
+
+gsap.from('#contactForm', {scrollTrigger: "#contact", opacity: 0, duration: 2});
+
+
     
