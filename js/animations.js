dbf8afbb381aa38c41a5df6d9eb8ebacc20b5855
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-10 03:10:42 +1100

    fix

---
+++ js/animations.js
@@ -0,0 +1,13 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('#welcomeText', { x: '100%', duration: 2, ease: 'elastic'});
+
+gsap.from('#profileImg', {scrollTrigger: "#aboutEndWave", opacity: 0, duration: 1, ease: 'power1'});
+gsap.from('#aboutMe', {scrollTrigger: "#aboutEndWave", opacity: 0, duration: 3, ease: 'power1'});
+
+gsap.from('.project', {scrollTrigger: "#projects", y: '300%', stagger: 0.3, ease: 'power1'});
+
+gsap.from('#contactForm', {scrollTrigger: "#contact", opacity: 0, duration: 2});
+
+
+    
